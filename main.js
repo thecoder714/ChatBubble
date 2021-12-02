0ddc7658f6dfebac2b8486f88b41018252d486f8
Aarush-Jha <75258277+Aarush-Jha@users.noreply.github.com>
2021-12-02 21:19:15 +0530

    changed location

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function addUser() {
     user = document.getElementById("loginBox").innerHTML;
     localStorage.setItem("user", user);
-    window.location = "/Chat%20Website/Login%20Pages/Main%20Pages/main-page.html";
+    window.location = "ChatBubble/main/main-page.html";
 }
