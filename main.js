8432d2ce56320c9d089460bee31e0293e3894262
Aarush-Jha <75258277+Aarush-Jha@users.noreply.github.com>
2021-12-02 21:31:19 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function addUser() {
     user = document.getElementById("loginBox").innerHTML;
     localStorage.setItem("user", user);
-    window.location = "main/main-page.html";
+    window.location = "/main-page.html";
 }
