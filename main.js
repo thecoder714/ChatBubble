1b662c0345093b112ebd52dbbcfd739769909a13
thecoder714 <75258277+thecoder714@users.noreply.github.com>
2022-02-01 14:33:41 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function addUser() {
     user = document.getElementById("loginBox").innerHTML;
     localStorage.setItem("user", user);
-    window.location = "ChatBubble/main-page.html";
+    window.location = "main-page.html";
 }
